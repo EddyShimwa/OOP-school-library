199b0d42c4dd494836bc0948edd8950b8300d202
EddyShimwa <shedpray16@gmail.com>
2023-03-09 13:24:53 +0200

    added the realationships

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,19 @@
+require_relative 'student_class'
+
 class Classroom
-    attr_accessor :label
+    attr_accessor :label, :students
 
     def initialize(label)
         @label = label
+        @students = []
+    end
+
+    def add_student(student)
+        @students << (student)
     end
 end
+
+classroom = Classroom.new('Class A')
+student = Student.new(classroom: classroom, name: 'John', age: 15, parent_permission: true)
+classroom.add_student(student)
+puts student.classroom.label #=> "Class A"
